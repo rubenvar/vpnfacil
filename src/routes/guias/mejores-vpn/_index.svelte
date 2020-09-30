08b7d2b4968d378c22de850aa82679ddff606140
Rubén <hey@rubenvara.io>
2020-09-30 11:41:31 +0200

    new post

---
+++ src/routes/guias/mejores-vpn/_index.svelte
@@ -0,0 +1,22 @@
+<table>
+  <thead>
+    <tr>
+      <th>Logo</th>
+      <th>VPN</th>
+      <th>Servidores</th>
+      <th>Países</th>
+      <th>Dispositivos</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each bestVpns as vpn}
+      <tr style="background-color: {vpn.color}">
+        <td>vpn.logo</td>
+        <td>vpn.name</td>
+        <td>vpn.servers</td>
+        <td>vpn.countries</td>
+        <td>vpn.devices</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
