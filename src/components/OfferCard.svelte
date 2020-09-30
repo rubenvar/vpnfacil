49b878818ef927fa0ed4b27f4651e24b3ae19baf
Rubén <hey@rubenvara.io>
2020-09-30 15:20:50 +0200

    new post - offers

--- src/components/OfferCard.svelte
+++ src/components/OfferCard.svelte
@@ -1,17 +1,128 @@
 <script>
+  import StarRating from 'svelte-star-rating';
   export let vpn;
+  export let subtitle;
+  export let comment;
+
+  function formatMoney(amount, currency = 'USD') {
+    const options = {
+      style: 'currency',
+      currency,
+      minimumFractionDigits: 2,
+    };
+
+    // if its a whole amount, leave off the .00
+    if (amount % 100 === 0) options.minimumFractionDigits = 0;
+
+    const formatter = new Intl.NumberFormat('es-ES', options);
+    return formatter.format(amount / 100);
+  }
 </script>
 
 <style lang="scss">
   article {
     border: 3px solid var(--primary500);
     border-radius: var(--cardRadius);
-    h2 {
-      color: firebrick;
+    padding: 25px;
+    margin: 60px 0;
+    background-color: var(--grey100);
+    box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.4);
+    transition: 0.3s;
+    &:hover {
+      transform: scale(1.02) rotate(-1deg);
+      box-shadow: 0 0 8px 5px rgba(0, 0, 0, 0.15);
+      background-color: white;
+    }
+    h3 {
+      margin: 0 0 15px;
+    }
+    .info {
+      display: grid;
+      grid-template-columns: 3fr 2fr;
+      gap: 30px;
+      .text {
+        ul {
+          li {
+            .tag {
+              color: var(--grey500);
+              font-size: 19px;
+            }
+          }
+        }
+        p {
+          margin: 0;
+          font-size: 18px;
+          color: var(--grey700);
+        }
+      }
+      .details {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        img {
+          display: block;
+          margin: 0 auto;
+          border-radius: 5px;
+          border: 1px solid transparent;
+          transition: all 0.3s;
+          &:hover {
+            transform: scale(1.01);
+            border-color: var(--primary500);
+          }
+        }
+        button {
+          transition: all 0.3s;
+          border-radius: 8px;
+          padding: 12px 20px;
+          border: none;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
+          color: white;
+          background-color: var(--primary700);
+          cursor: pointer;
+          &:hover {
+            background-color: var(--secondary700);
+          }
+        }
+      }
     }
   }
 </style>
 
 <article>
-  <h2>{vpn.name}</h2>
+  <h3>{vpn.name}: {subtitle}</h3>
+  <div class="info">
+    <div class="text">
+      <ul>
+        <li>
+          <span class="tag">Descuento:</span>
+          {Math.round(100 - (vpn.plan3Pricing * 100) / vpn.plan1Pricing)}%
+        </li>
+        <li>
+          <span class="tag">Precio por mes:</span>
+          {formatMoney(vpn.plan3Pricing, vpn.plan3Currency)}
+        </li>
+        <li><span class="tag">Duración:</span> {vpn.plan3Length / 12} años</li>
+        <li>
+          <span class="tag">Total que pagas ahora:</span>
+          {formatMoney(vpn.plan3Pricing * vpn.plan3Length, vpn.plan3Currency)}
+        </li>
+      </ul>
+      {#if comment}
+        <p>{comment}</p>
+      {/if}
+    </div>
+    <div class="details">
+      <a href={vpn.link} target="_blank" rel="nofollow">
+        <img src="/vpns/{vpn.id}.jpg" alt="Logo de {vpn.name}" />
+      </a>
+      <StarRating
+        id={vpn.id}
+        rating={vpn.rating / 20}
+        config={{ size: 30, fullColor: '#ffc107', showText: true }}
+        style="" />
+      <a href={vpn.link} target="_blank" rel="nofollow"><button>Mira la oferta
+          <strong>actualizada</strong></button></a>
+    </div>
+  </div>
 </article>
