1254b5f241885b159720945e45b426d3650704ab
Rubén <hey@rubenvara.io>
2020-09-30 16:26:02 +0200

    responsive styling for blog

--- src/routes/guias/_guias-layout.svelte
+++ src/routes/guias/_guias-layout.svelte
@@ -11,14 +11,22 @@
   });
 </script>
 
-<style>
+<style lang="scss">
   div {
-    max-width: var(--maxWidthBlog);
-    margin: 0 auto;
+    max-width: 100%;
+    margin-left: 20px;
+    margin-right: 20px;
+    @media only screen and (min-width: 760px) {
+      max-width: var(--maxWidthBlog);
+      margin: 0 auto;
+    }
   }
 
   header {
-    margin: 50px 0 40px;
+    margin: 30px 0 40px;
+    @media only screen and (min-width: 760px) {
+      margin-top: 50px;
+    }
   }
 
   h1 {
@@ -26,7 +34,12 @@
     font-family: var(--specialFont);
     font-weight: 400;
     color: var(--primary600);
-    font-size: 46px;
+    font-size: 32px;
+    @media only screen and (min-width: 640px) {
+      font-size: 46px;
+    }
+    @media only screen and (min-width: 760px) {
+    }
   }
 
   header p {
@@ -35,8 +48,11 @@
   }
 
   article {
-    font-size: 20px;
+    font-size: 18px;
     line-height: 1.65;
+    @media only screen and (min-width: 640px) {
+      font-size: 20px;
+    }
   }
 </style>
 
--- src/routes/guias/index.svelte
+++ src/routes/guias/index.svelte
@@ -10,26 +10,43 @@
   export let posts;
 </script>
 
-<style>
+<style lang="scss">
   .container {
-    max-width: var(--maxWidthBlog);
-    margin: 0 auto;
+    max-width: 100%;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin: 40px 20px;
+    @media only screen and (min-width: 760px) {
+      max-width: var(--maxWidthBlog);
+      margin-left: auto;
+      margin-right: auto;
+    }
   }
 
   h1 {
-    font-size: 36px;
-    margin-bottom: 60px;
+    font-size: 28px;
+    margin-bottom: 40px;
+    @media only screen and (min-width: 640px) {
+      font-size: 36px;
+      margin-bottom: 60px;
+    }
   }
 
   article {
-    margin-bottom: 52px;
+    margin-bottom: 40px;
+    @media only screen and (min-width: 640px) {
+      margin-bottom: 52px;
+    }
   }
 
   h2 {
     font-family: var(--specialFont);
     font-weight: 700;
-    font-size: 22px;
+    font-size: 21px;
     display: inline;
+    @media only screen and (min-width: 640px) {
+      font-size: 22px;
+    }
   }
 
   h2 a {
@@ -49,9 +66,12 @@
   span,
   .excerpt {
     color: var(--grey500);
-    font-size: 20px;
+    font-size: 18px;
     display: inline;
     line-height: 1.7;
+    @media only screen and (min-width: 640px) {
+      font-size: 20px;
+    }
   }
 </style>
 
@@ -60,7 +80,7 @@
 </svelte:head>
 
 <div class="container">
-  <h1>Guías</h1>
+  <h1>Todas las Guías sobre VPN:</h1>
   {#each posts as post}
     <article>
       <h2><a rel="prefetch" href="guias/{post.slug}">{post.title}</a></h2>
