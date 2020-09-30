1e817cdec591e651fe667c6759f9ea35dce2d6d9
Rubén <hey@rubenvara.io>
2020-09-30 18:03:35 +0200

    dont use scss in mdsvex layout

--- src/routes/guias/_guias-layout.svelte
+++ src/routes/guias/_guias-layout.svelte
@@ -11,12 +11,14 @@
   });
 </script>
 
-<style lang="scss">
+<style>
   div {
     max-width: 100%;
     margin-left: 20px;
     margin-right: 20px;
-    @media only screen and (min-width: 760px) {
+  }
+  @media only screen and (min-width: 760px) {
+    div {
       max-width: var(--maxWidthBlog);
       margin: 0 auto;
     }
@@ -24,7 +26,9 @@
 
   header {
     margin: 30px 0 40px;
-    @media only screen and (min-width: 760px) {
+  }
+  @media only screen and (min-width: 760px) {
+    header {
       margin-top: 50px;
     }
   }
@@ -35,10 +39,10 @@
     font-weight: 400;
     color: var(--primary600);
     font-size: 32px;
-    @media only screen and (min-width: 640px) {
+  }
+  @media only screen and (min-width: 640px) {
+    h1 {
       font-size: 46px;
-    }
-    @media only screen and (min-width: 760px) {
     }
   }
 
@@ -50,7 +54,9 @@
   article {
     font-size: 18px;
     line-height: 1.65;
-    @media only screen and (min-width: 640px) {
+  }
+  @media only screen and (min-width: 640px) {
+    article {
       font-size: 20px;
     }
   }
