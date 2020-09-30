063c60fdd83b1301e317983fd447089dbd6986e5
Rubén <hey@rubenvara.io>
2020-09-30 12:53:33 +0200

    new post - offers

---
+++ src/components/OfferCard.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let vpn;
+</script>
+
+<style>
+  article {
+    border: 3px solid var(--primary500);
+    border-radius: var(--cardRadius);
+  }
+</style>
+
+<article>
+  <h2>{vpn.name}</h2>
+</article>
