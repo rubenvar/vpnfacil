1059a785baaab19d2db2bcdc0823cc3dc5000775
Rubén <hey@rubenvara.io>
2020-10-06 12:36:18 +0200

    add trailing slash 🙉

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 
 const homePage =
-  '<url><loc>https://vpnfacil.com</loc><priority>1</priority></url>';
+  '<url><loc>https://vpnfacil.com/</loc><priority>1</priority></url>';
 const blogPage =
-  '<url><loc>https://vpnfacil.com/guias</loc><priority>0.8</priority></url>';
+  '<url><loc>https://vpnfacil.com/guias/</loc><priority>0.8</priority></url>';
 
 const render = posts => `<?xml version="1.0" encoding="UTF-8" ?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -12,7 +12,7 @@ const render = posts => `<?xml version="1.0" encoding="UTF-8" ?>
     ${posts
       .map(
         post => `<url>
-                 <loc>${`https://vpnfacil.com/guias/${post.slug}`}</loc>
+                 <loc>${`https://vpnfacil.com/guias/${post.slug}/`}</loc>
                  <lastmod>${post.date}</lastmod>
                  <priority>0.6</priority>
                 </url>`
