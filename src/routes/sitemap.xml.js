8819a4c2eb128a9d7888c317dbe2a4f4cbc0c868
Rubén <hey@rubenvara.io>
2020-10-28 15:46:08 +0100

    try to fix sitemap

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -4,7 +4,7 @@ import { awsConfig } from '../../config';
 const homePage =
   '<url><loc>https://vpnfacil.com/</loc><priority>1</priority></url>';
 const blogPage =
-  '<url><loc>https://vpnfacil.com/guias/</loc><priority>0.8</priority></url>';
+  '<url><loc>https://vpnfacil.com/guias/</loc><priority>0.7</priority></url>';
 
 const render = (posts, vpns) => `<?xml version="1.0" encoding="UTF-8" ?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -23,7 +23,7 @@ const render = (posts, vpns) => `<?xml version="1.0" encoding="UTF-8" ?>
       .map(
         vpn => `<url>
                   <loc>${`https://vpnfacil.com/vpn/${vpn.slug}/`}</loc>
-                  <priority>0.7</priority>
+                  <priority>0.8</priority>
                 </url>`
       )
       .join('')}
