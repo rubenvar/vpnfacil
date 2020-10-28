0b8913dad200bb3fe6229ff426e1d9a8fe3bacf1
Rubén <hey@rubenvara.io>
2020-10-28 11:58:54 +0100

    try to integrate sentry, not working 🤷‍♂️

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,21 @@
 <script>
+  // import * as Sentry from '@sentry/browser';
+  // import { Integrations } from '@sentry/tracing';
   import { stores } from '@sapper/app';
   import GoogleAnalytics from '../components/GoogleAnalytics.svelte';
   import Header from '../components/Header.svelte';
   import Hero from '../components/Hero.svelte';
   import Footer from '../components/Footer.svelte';
+
+  // not working, on error app just crashes 🤷‍♂️
+  // Sentry.init({
+  //   dsn:
+  //     'https://1ced485c97f04680b3576700891e7535@o221871.ingest.sentry.io/5496057',
+  //   integrations: [new Integrations.BrowserTracing()],
+
+  //   // We recommend adjusting this value in production, or using tracesSampler for finer control
+  //   tracesSampleRate: 1.0,
+  // });
 
   const dev = process.env.NODE_ENV === 'development';
   const { page } = stores();
