b0f47cb73597d462985686ac5bfcab40a70ace21
Rubén <hey@rubenvara.io>
2020-10-30 22:01:36 +0100

    helper fn to call Screenshot lambda and after store the screenshot

---
+++ src/utils/takeNewScreenshot.js
@@ -0,0 +1,42 @@
+// import fetch from 'node-fetch';
+import axios from 'axios';
+
+async function postScreenshot(id, screenshot) {
+  const resp = await axios
+    .post(`${process.env.ENDPOINT}/update-screenshot`, {
+      id,
+      screenshot,
+    })
+    .then(res => {
+      console.log(resp);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+export async function takeNewScreenshot(id, baseUrl) {
+  console.log(`new screenshot called, for VPN id ${id}. Let's see...`);
+
+  // call lambda (async)
+  const body = {
+    url: baseUrl,
+    cloudinaryFolder: 'vpnf/screenshots',
+    fileName: id,
+    width: 1920,
+    height: 1080,
+  };
+  await axios
+    .post(process.env.SCREENSHOT_LAMBDA, body)
+    .then(({ status, data }) => {
+      console.log(status);
+      console.log(data);
+      const screenshot = data.url;
+      postScreenshot(id, screenshot);
+    })
+    .catch(error => {
+      console.log('⚠️ 🐛 ⚠️ 🐛 ⚠️ 🐛 There was an error ↓');
+      console.log(error);
+      console.log('⚠️ 🐛 ⚠️ 🐛 ⚠️ 🐛 There was an error ↑');
+    });
+}
