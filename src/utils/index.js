a5fe6cfe13b7aca040f8511bd6ccdfe8335f9289
Rubén <hey@rubenvara.io>
2020-10-30 22:04:25 +0100

    helper fn to call Screenshot lambda and store screenshot 📷

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,5 @@ export { countReps } from './countReps';
 export { formatMoney } from './formatMoney';
 export { formatNumber } from './formatNumber';
 export { formatPercent } from './formatPercent';
+export { insertString } from './insertString';
+export { takeNewScreenshot } from './takeNewScreenshot';
