a4e375cae93b6400706e972237c683f08cf64bd2
Rubén <hey@rubenvara.io>
2020-11-04 23:22:34 +0100

    create the Row component for table view

--- src/components/Row.svelte
+++ src/components/Row.svelte
@@ -33,45 +33,55 @@
     border: 2px solid var(--primary300);
     border-radius: var(--cardRadius);
     color: #444;
-    overflow: hidden;
+    // overflow: hidden;
     display: grid;
-    grid-template-columns: auto 1fr 1fr 2fr auto;
+    grid-template-columns: auto 0.6fr 1fr 2fr 0.4fr;
     gap: 15px;
     align-items: center;
     transition: all 0.3s;
     font-family: var(--specialFont);
-    &.emphasis {
-      box-shadow: 0 0 0 5px #ffcf00;
-      border: none;
-    }
-    // &:hover {
-    //   transform: scale(1.02);
-    //   img {
-    //     filter: saturate(75%) blur(0px) opacity(90%);
-    //   }
-    //   h2 {
-    //     color: var(--vpn-color);
-    //   }
-    //   .go {
-    //     background-color: var(--primary300);
-    //     &:hover {
-    //       background-color: var(--primary500);
-    //     }
-    //   }
+    max-width: 100%;
+    // &.emphasis {
+    //   box-shadow: 0 0 0 5px #ffcf00;
+    //   border: none;
     // }
+    &:hover {
+      transform: scale(1.02);
+      img {
+        filter: saturate(75%) blur(0px) opacity(90%);
+      }
+      h2 {
+        color: var(--vpn-color);
+      }
+      .go {
+        background-color: var(--primary300);
+        &:hover {
+          background-color: var(--primary500);
+        }
+      }
+    }
   }
   img {
     width: 40px;
     border-radius: 20%;
     filter: saturate(50%) blur(0.75px) opacity(70%);
     transition: all 0.3s;
+    margin-left: 7px;
   }
-  h2 {
-    position: relative;
-    transition: all 0.3s;
-    font-size: 25px;
-    margin: 0;
-    font-family: var(--mainFont);
+  .title {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    h2 {
+      position: relative;
+      transition: all 0.3s;
+      font-size: 21px;
+      margin: 0;
+      font-family: var(--mainFont);
+      @media only screen and (min-width: 960px) {
+        font-size: 25px;
+      }
+    }
   }
   a {
     text-decoration: none;
@@ -83,87 +93,99 @@
   }
   .all-details {
     display: grid;
-    // grid-template-columns: repeat(6, 1fr);
-    grid-template-columns: repeat(6, 110px);
-    gap: 15px;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 7px;
     align-items: center;
+    @media only screen and (min-width: 960px) {
+      grid-template-columns: repeat(4, 1fr);
+      gap: 15px;
+    }
     .info {
-      display: flex;
+      display: unset;
       flex-wrap: wrap;
       text-align: center;
       justify-content: center;
+      @media only screen and (min-width: 960px) {
+        display: flex;
+      }
       .tag {
         font-weight: 300;
         width: 100%;
       }
     }
   }
-  .go {
-    background-color: var(--primary100);
-    text-align: center;
-    border-radius: 0;
-    padding: 16px;
-    transition: all 0.3s;
-    font-size: 20px;
-    @media only screen and (min-width: 580px) {
-      font-size: unset;
+  .go-link {
+    height: 100%;
+    .go {
+      background-color: var(--primary100);
+      transition: all 0.3s;
+      font-size: 20px;
+      height: 100%;
+      text-align: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-top-right-radius: 13px;
+      border-bottom-right-radius: 13px;
+      @media only screen and (min-width: 580px) {
+        font-size: unset;
+      }
     }
   }
 </style>
 
-<article {id} style="--vpn-color: {color}" class={i < 3 ? 'emphasis' : ''}>
-  <img src="/vpns/{id}.jpg" alt="Logo de {name}" />
+<a href="/vpn/{slug}">
+  <article {id} style="--vpn-color: {color}" class={i < 3 ? 'emphasis' : ''}>
+    <img src="/vpns/{id}.jpg" alt="Logo de {name}" />
+    <div class="title">
+      <h2><a href="/vpn/{slug}/">{name}</a></h2>
 
-  <h2><a href="/vpn/{slug}/">{name}</a></h2>
+      {#if rating}
+        <StarRating
+          {id}
+          rating={rating / 20}
+          config={{ fullColor: '#ffd65a', emptyColor: '#a9a9a9', size: 16 }}
+          style="justify-content: center;margin: 7px 0;" />
+      {/if}
+    </div>
+    <Numbers {numbers} isRow />
 
-  <Numbers {numbers} />
+    <div class="all-details">
+      {#if moneyBack === 'yes'}
+        <span class="info">
+          {moneyBackDays}
+          días
+          <span class="tag">devolución</span>
+        </span>
+      {:else if moneyBack === 'no'}
+        <span class="info">No<span class="tag">devolución</span></span>
+      {:else}<span class="info">-</span>{/if}
 
-  <div class="all-details">
-    {#if moneyBack === 'yes'}
-      <span class="info">
-        {moneyBackDays}
-        días
-        <span class="tag">devolución</span>
-      </span>
-    {:else if moneyBack === 'no'}
-      <span class="info">No<span class="tag">devolución</span></span>
-    {:else}<span class="info" />{/if}
+      {#if appLanguage !== ''}
+        <span class="info">{#if spanish}
+            <span class="tag">en</span>
+            español
+          {:else}<span class="tag">en</span> inglés{/if}</span>
+      {:else}<span class="info">-</span>{/if}
 
-    {#if appLanguage !== ''}
-      <span class="info">{#if spanish}en español{:else}en inglés{/if}</span>
-    {:else}<span class="info" />{/if}
+      {#if compatIndex}
+        <span class="info">
+          <span class="tag">compatibilidad</span>
+          {compatIndex < 6 ? 'baja' : compatIndex > 5 && compatIndex < 12 ? 'media' : 'alta'}
+        </span>
+      {:else}<span class="info">-</span>{/if}
 
-    {#if compatIndex}
-      <span class="info">
-        {compatIndex < 6 ? 'baja' : compatIndex > 5 && compatIndex < 12 ? 'media' : 'alta'}
-        <span class="tag">compatibilidad</span>
-      </span>
-    {:else}<span class="info" />{/if}
+      {#if noLogs === 'yes'}
+        <span class="info">No guarda logs</span>
+      {:else if noLogs === 'no'}
+        <span class="info">Puede guardar logs</span>
+      {:else}<span class="info">-</span>{/if}
+    </div>
 
-    {#if noLogs === 'yes'}
-      <span class="info">No guarda logs</span>
-    {:else if noLogs === 'no'}
-      <span class="info">Puede guardar logs</span>
-    {:else}<span class="info" />{/if}
-
-    {#if p2p === 'yes'}
-      <span class="info">Compatible con P2P</span>
-    {:else if p2p === 'no'}
-      <span class="info">No compatible con P2P</span>
-    {:else}<span class="info" />{/if}
-
-    {#if rating}
-      <StarRating
-        {id}
-        rating={rating / 20}
-        config={{ fullColor: '#ffc107', showText: true }}
-        style="justify-content: center;margin: 20px 0 0;" />
-    {:else}<span class="info" />{/if}
-  </div>
-
-  {#if slug}
-    <a href="/vpn/{slug}/" title="Ir a {name}">
-      <div class="go"><span>Ver más info</span></div>
-    </a>
-  {/if}
-</article>
+    {#if slug}
+      <a class="go-link" href="/vpn/{slug}/" title="Ir a {name}">
+        <div class="go"><span>Ver más info</span></div>
+      </a>
+    {/if}
+  </article>
+</a>
